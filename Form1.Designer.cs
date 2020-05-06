6e2f0e7fa5d59a531d4982f040a14760b1f96e72
woykiz <36074624+woykiz@users.noreply.github.com>
2020-05-06 17:54:00 +0300

    Commit bug

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace LaboratornayaRabota
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.seedTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rTextBox = new System.Windows.Forms.TextBox();
+            this.sTextBox = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.seedTextBox);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.rTextBox);
+            this.groupBox1.Controls.Add(this.sTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(196, 426);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Параметры";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Зерно";
+            // 
+            // seedTextBox
+            // 
+            this.seedTextBox.Location = new System.Drawing.Point(19, 133);
+            this.seedTextBox.Name = "seedTextBox";
+            this.seedTextBox.Size = new System.Drawing.Size(159, 20);
+            this.seedTextBox.TabIndex = 5;
+            this.seedTextBox.Text = "1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 36);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "поехали";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Повтроения";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Сигма";
+            // 
+            // rTextBox
+            // 
+            this.rTextBox.Location = new System.Drawing.Point(19, 88);
+            this.rTextBox.Name = "rTextBox";
+            this.rTextBox.Size = new System.Drawing.Size(159, 20);
+            this.rTextBox.TabIndex = 1;
+            this.rTextBox.Text = "1";
+            // 
+            // sTextBox
+            // 
+            this.sTextBox.Location = new System.Drawing.Point(19, 46);
+            this.sTextBox.Name = "sTextBox";
+            this.sTextBox.Size = new System.Drawing.Size(159, 20);
+            this.sTextBox.TabIndex = 0;
+            this.sTextBox.Text = "0.5";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(607, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(196, 426);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(214, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(383, 426);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 283);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(132, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Использовать зерно";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 355);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Сбросить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 306);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(172, 34);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "инициализировать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Лабораторочка";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox rTextBox;
+        private System.Windows.Forms.TextBox sTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox seedTextBox;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace LaboratornayaRabota
+{
+    public partial class Form1 : Form
+    {
+        Random random { get; set; }
+        List<double> expirements { get; set; } = new List<double>();
+        double function(double x, double d)
+        {
+            return Math.Sqrt(-2 * d * d * Math.Log(1 - x));
+        }
+
+        double p(double y, double d)
+        {
+            return y / (d * d) * Math.Exp(-y * y / (2 * d * d));
+        }
+
+        double D => Convert.ToDouble(sTextBox.Text.Replace('.', ','));
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.ColumnCount = 1;
+            dataGridView1.Columns[0].Name = "X";
+
+            button1.Enabled = false;
+            chart1.Series.Clear();
+
+        }
+
+        void DrawChart()
+        {
+            chart1.Series.Clear();
+            var ser = new Series();
+            ser.ChartType = SeriesChartType.Line;
+
+            ser.Name = "Плотность";
+
+            for(double x = 0;  x < 10; x += 0.01)
+            {
+                ser.Points.AddXY(x, p(x, D));
+            }
+
+            var bar = new Series();
+            bar.ChartType = SeriesChartType.Line;
+
+            bar.Name = "Эксперименты";
+
+
+            chart1.Series.Add(ser);
+            chart1.Series.Add(bar);
+        }
+
+        async private void button1_Click(object sender, EventArgs e)
+        {
+            this.button1.Enabled = false;
+            this.button2.Enabled = false;
+            this.button3.Enabled = false;
+
+            await Task.Run(new Action(() =>
+            {
+                var count = Convert.ToInt32(rTextBox.Text);
+
+                for (int i = 0; i < count; ++i)
+                {
+                    var d = D;
+                    var number = random.NextDouble();
+                    var X = function(number, d);
+
+                    expirements.Add(X);
+                }
+                expirements = expirements.OrderBy(x => x).ToList();
+
+            }));
+
+            
+            dataGridView1.Rows.Clear();
+            foreach (var n in expirements)
+                dataGridView1.Rows.Add(n + "");
+
+            BuildData();
+
+            this.button1.Enabled = true;
+            this.button2.Enabled = true;
+            this.button3.Enabled = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            expirements.Clear();
+            dataGridView1.Rows.Clear();
+
+            random = null;
+            button1.Enabled = false;
+            button3.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                random = new Random(Convert.ToInt32(seedTextBox.Text));
+            }
+            else
+            {
+                random = new Random();
+            }
+
+            DrawChart();
+            button1.Enabled = true;
+            button3.Enabled = false;
+        }
+
+        void BuildData()
+        {
+            chart1.Series[1].Points.Clear();
+            int k = 0;
+            List<double> points = new List<double>();
+            for (double x = 0; x < 10; x += 0.01)
+            {
+                foreach(var n in expirements)
+                {
+                    if (Math.Abs(n - x) <= 0.8)
+                    {
+                        ++k;
+                    }
+                }
+
+                points.Add(k);
+                k = 0;
+            }
+
+            var max = points.Max();
+            int i = 0;
+            for (double x = 0; x < 10; x += 0.01, ++i)
+            {
+                chart1.Series[1].Points.AddXY(x, points[i] / max);
+            }
+        }
+    }
+}
